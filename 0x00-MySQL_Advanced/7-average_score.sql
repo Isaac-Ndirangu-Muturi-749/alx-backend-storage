4283858fedbe35ccdc2fd8319f4416abcaa80d44
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-06-16 04:35:40 +0300

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,6 +4,8 @@
 
 -- Procedure ComputeAverageScoreForUser is taking 1 input:
 -- user_id, a users.id value (you can assume user_id is linked to an existing users)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(
@@ -11,16 +13,8 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
 )
 BEGIN
     DECLARE avg_score FLOAT;
-
-    -- Compute the average score for the given user
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Update the user's average score
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
+    SET avg_score = (SELECT AVG(score) FROM corrections WHERE user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
 END//
 
 DELIMITER ;

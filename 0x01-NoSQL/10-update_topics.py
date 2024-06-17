c233e4037a483c0583dc91fdf3e3472d5b18349f
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-06-17 15:13:19 +0300

    Update 10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module 10-update_topics
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update the topics of a school document based on the name
+    :param mongo_collection: pymongo collection object
+    :param name: string, the school name to update
+    :param topics: list of strings, the list of topics approached in the school
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )

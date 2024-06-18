a5b7c44e6c04de7586c8d4e5ca37d29137884fee
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-06-18 08:55:40 +0300

    Update 12-log_stats.py

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module 10-update_topics
+"""
+
+from pymongo import MongoClient
+
+
+def log_stats():
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx_collection = db.nginx
+
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
+
+if __name__ == "__main__":
+    log_stats()
